87f3cdc37f3486f8b19d1f0d5f3e21c47a1bfe07
tusharsahu2599 <tksahu1234@gmail.com>
2022-03-26 19:06:27 +0530

    Add project

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import Axios from "axios";
+
+//components
+
+
+import CityComponent from "./components/CityComponent.jsx"
+import WeatherComponent from "./components/WeatherInfo";
+
+export const WeatherIcons = {
+  "01d": "/react-weather-app/icons/sunny.svg",
+  "01n": "/react-weather-app/icons/night.svg",
+  "02d": "/react-weather-app/icons/day.svg",
+  "02n": "/react-weather-app/icons/cloudy-night.svg",
+  "03d": "/react-weather-app/icons/cloudy.svg",
+  "03n": "/react-weather-app/icons/cloudy.svg",
+  "04d": "/react-weather-app/icons/perfect-day.svg",
+  "04n": "/react-weather-app/icons/cloudy-night.svg",
+  "09d": "/react-weather-app/icons/rain.svg",
+  "09n": "/react-weather-app/icons/rain-night.svg",
+  "10d": "/react-weather-app/icons/rain.svg",
+  "10n": "/react-weather-app/icons/rain-night.svg",
+  "11d": "/react-weather-app/icons/storm.svg",
+  "11n": "/react-weather-app/icons/storm.svg",
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 380px;
+  padding: 20px 10px;
+  margin: auto;
+  border-radius: 4px;
+  box-shadow: 0 3px 6px 0 #555;
+  background: #EFFFFD;
+  font-family: Montserrat;
+
+`;
+
+const Label = styled.span`
+  color: black;
+  margin: 20px auto;
+  font-size: 18px;
+  font-weight: bold;
+`;
+const CloseButton = styled.span`
+  padding: 2px 3px;
+  background-color: black;
+  border-radius: 50%;
+  color: white;
+  position: absolute;
+`;
+
+function App() {
+  const [city, updateCity] = useState();
+  const [weather, updateWeather] = useState();
+  const fetchWeather = async (e) => {
+    e.preventDefault();
+    const response = await Axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8744b2e166987ccfe85dcedaf82ab3eb`,
+    );
+    updateWeather(response.data);
+  };
+  return (
+    <Container>
+      <CloseButton/>
+
+      <Label>Weather App</Label>
+      {city && weather ? (
+        <WeatherComponent weather={weather} city={city} />
+      ) : (
+        <CityComponent updateCity={updateCity} fetchWeather={fetchWeather} />
+      )}
+    </Container>
+    
+  );
+}
+
+export default App;
